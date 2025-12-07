9f013ac6734630beffa7e1ff9bc5ced45e40db62
Keshav sharma <sharmakeshav54126@gmail.com>
2025-12-07 00:00:28 +0000

    added a renderer and a window for our browser using tkinter

--- browser.py
+++ browser.py
@@ -1,108 +1,30 @@
-import socket
-import sys
-import ssl
-import os
-from typing import Optional
+from url import URL
+import tkinter
+WIDTH = 800
+HEIGHT = 600
+HSTEP, VSTEP = 13, 18
 
 
-class URL:
-    def __init__(self, url: Optional[str]):
-        if url and url.startswith("file://"):
-            self.scheme, self.path = url.split("://", 1)
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
 
-        elif url is None:
-            self.scheme = "file"
-            self.path = "/home/keshav/dragoon/temp.txt"
-
-        else:
-            self.scheme, url = url.split("://", 1)
-            assert self.scheme in ["http", "https"]
-            if self.scheme == "http":
-                self.port = 80
-            if self.scheme == "https":
-                self.port = 443
-            if "/" not in url:
-                url += "/"
-            self.host, url = url.split("/", 1)
-            if ":" in self.host:
-                self.host, port = self.host.split(":", 1)
-                self.port = int(port)
-            self.path = "/"+url
-
-    def add_header(self, request, headers: dict[str, str]):
-        for key in headers:
-            header_key = key
-            header_value = headers.get(key, "")
-            request += f"{key}: {header_value}\r\n"
-
-        request += "\r\n"
-        return request
-
-    def request(self):
-        if self.scheme == "file":
-            self.show()
-            return
-        if self.scheme in ["http", "https"]:
-            ctx = ssl.create_default_context()
-
-            s = socket.socket(
-                family=socket.AF_INET,
-                type=socket.SOCK_STREAM,
-                proto=socket.IPPROTO_TCP
-            )
-
-            if self.scheme == "https":
-                s = ctx.wrap_socket(s, server_hostname=self.host)
-
-            s.connect((self.host, self.port))
-
-            request = "GET {} HTTP/1.1\r\n".format(self.path)
-
-            headers = {
-                "Host": self.host,
-                "Connection": "close"
-            }
-            request = self.add_header(request=request, headers=headers)
-
-            s.send(request.encode("utf8"))
-            response = s.makefile("r", encoding="utf8", newline="\r\n")
-            statusline = response.readline()
-            version, status, explanation = statusline.split(" ", 2)
-            response_headers = {}
-            while True:
-                line = response.readline()
-                if line == "\r\n":
-                    break
-                header, value = line.split(":", 1)
-                response_headers[header.casefold()] = value.strip()
-
-            self.content = response.read()
-            s.close()
-            self.show()
-            return
-        else:
-            return
-
-    def show(self):
-        if self.scheme == "file" and self.path:
-            filepath = os.path.abspath(self.path)
-            with open(file=filepath) as f:
-                data = f.read()
-            print(data)
-
-        else:
-            in_tag = False
-            for char in body:
-                if char == "<":
-                    in_tag = True
-                elif char == ">":
-                    in_tag = False
-                elif not in_tag:
-                    print(char, end="")
-
-
-def load(url):
-    url.request()
+    def load(self, url):
+        url.request()
+        self.canvas.create_rectangle(10, 20, 400, 300)
+        self.canvas.create_oval(100, 100, 150, 150)
+        self.canvas.create_text(200, 150, text="Hi!")
+        text = url.show()
+        cursor_x, cursor_y = HSTEP, VSTEP
+        for c in text:
+            self.canvas.create_text(cursor_x, cursor_y, text=c)
+            cursor_x += HSTEP
 
 
 if __name__ == "__main__":
@@ -110,5 +32,6 @@ if __name__ == "__main__":
     url = None
     if len(sys.argv) > 1:
         url = sys.argv[1]
-
-    load(URL(url=url))
+    browser = Browser()
+    browser.load(URL(url=url))
+    tkinter.mainloop()
---
+++ url.py
@@ -0,0 +1,104 @@
+import socket
+import sys
+import ssl
+import os
+from typing import Optional
+
+
+class URL:
+    def __init__(self, url: Optional[str]):
+        if url and url.startswith("file://"):
+            self.scheme, self.path = url.split("://", 1)
+
+        elif url is None:
+            self.scheme = "file"
+            self.path = "/home/keshav/dragoon/temp.txt"
+
+        else:
+            self.scheme, url = url.split("://", 1)
+            assert self.scheme in ["http", "https"]
+            if self.scheme == "http":
+                self.port = 80
+            if self.scheme == "https":
+                self.port = 443
+            if "/" not in url:
+                url += "/"
+            self.host, url = url.split("/", 1)
+            if ":" in self.host:
+                self.host, port = self.host.split(":", 1)
+                self.port = int(port)
+            self.path = "/"+url
+
+    def add_header(self, request, headers: dict[str, str]):
+        for key in headers:
+            header_key = key
+            header_value = headers.get(key, "")
+            request += f"{key}: {header_value}\r\n"
+
+        request += "\r\n"
+        return request
+
+    def request(self):
+        if self.scheme == "file":
+            self.show()
+            return
+        if self.scheme in ["http", "https"]:
+            ctx = ssl.create_default_context()
+
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP
+            )
+
+            if self.scheme == "https":
+                s = ctx.wrap_socket(s, server_hostname=self.host)
+
+            s.connect((self.host, self.port))
+
+            request = "GET {} HTTP/1.1\r\n".format(self.path)
+
+            headers = {
+                "Host": self.host,
+                "Connection": "close"
+            }
+            request = self.add_header(request=request, headers=headers)
+
+            s.send(request.encode("utf8"))
+            response = s.makefile("r", encoding="utf8", newline="\r\n")
+            statusline = response.readline()
+            version, status, explanation = statusline.split(" ", 2)
+            response_headers = {}
+            while True:
+                line = response.readline()
+                if line == "\r\n":
+                    break
+                header, value = line.split(":", 1)
+                response_headers[header.casefold()] = value.strip()
+
+            self.content = response.read()
+            s.close()
+            self.show()
+            return
+        else:
+            return
+
+    def show(self):
+        if self.scheme == "file" and self.path:
+            filepath = os.path.abspath(self.path)
+            with open(file=filepath) as f:
+                data = f.read()
+            print(data)
+            return
+
+        else:
+            text = ""
+            in_tag = False
+            for char in self.content:
+                if char == "<":
+                    in_tag = True
+                elif char == ">":
+                    in_tag = False
+                elif not in_tag:
+                    text += char
+            return text
